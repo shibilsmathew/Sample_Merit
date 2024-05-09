cfe257a920a8f3669b14615243fbd92d6550d8dc
Merit <shibilsnathew2002@gmail.com>
2024-05-10 00:51:11 +0530

    Initial Commit (Project initial skeleton code)

---
+++ v1.py
@@ -0,0 +1,47 @@
+import sys
+
+class Task:
+    pass
+
+class TaskTracker:
+    pass
+
+def main():
+
+    task_tracker = TaskTracker()
+
+if __name__ == "__main__":
+
+    task_tracker = TaskTracker()
+
+    while True:
+        print("\nTask Tracker Menu:")
+        print("1. Add Task")
+        print("2. Schedule Task")
+        print("3. Delete Task")
+        print("4. List Tasks")
+        print("5. List Task with Schedules")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            pass
+
+        elif choice == '2':
+            pass
+
+        elif choice == '3':
+            pass
+
+        elif choice == '4':
+            pass
+
+        elif choice == '5':
+            pass
+
+        else:
+            print("Invalid choice. Please enter a number between 1 and 6.")
+
+if __name__ == "__main__":
+    main()
